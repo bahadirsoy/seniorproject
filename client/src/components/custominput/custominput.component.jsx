2a913c5eba699354741b708d7bd7dd07de41829e
Bahadir Ustabasi <oxikairos@hotmail.com>
2022-03-10 22:39:59 +0300

    custom input is added, sign in page is added

---
+++ client/src/components/custominput/custominput.component.jsx
@@ -0,0 +1,37 @@
+
+//import react
+import React from 'react';
+
+//import styles
+import './custominput.styles.css'
+
+//import reactstrap components
+import { InputGroup, Form, FormControl } from 'react-bootstrap'
+
+
+function CustomInput(props){
+    return(
+        <div>
+            {
+                props.labelName ?
+                <Form.Label htmlFor={props.formId}> {props.labelName} </Form.Label> :
+                null
+            }
+            <InputGroup className="mb-3">
+
+                <InputGroup.Text 
+                    id={props.inputNameId}>
+                    {props.inputName}
+                </InputGroup.Text>
+
+                <FormControl 
+                    id={props.formId} 
+                    aria-describedby={props.inputNameId}
+                    placeholder={props.placeholder}
+                />
+            </InputGroup>
+        </div>
+    )
+}
+
+export default CustomInput;
--- client/src/pages/signinpage/signinpage.jsx
+++ client/src/pages/signinpage/signinpage.jsx
@@ -5,11 +5,45 @@ import React from 'react';
 //import styles
 import './signinpage.styles.css'
 
+//import custom components
+import CustomInput from '../../components/custominput/custominput.component';
+
+//import react strap components
+import { Container, Row, Col } from 'react-bootstrap';
+
 
 function SignInPage(){
     return(
-        <div>
-            Sign In Page
+        <div className='mt-5'>
+
+
+            <Container>
+                <Row>
+                    
+                    <h1 className='mb-5'>Sign In Page</h1>
+
+                    <Col>
+                        
+                    </Col>
+
+                    <Col xs={10}>
+                        <CustomInput
+                            //labelName = "Username"
+                            inputName = "Username"
+                            placeholder = "Enter username"
+                            formId = "form-username"
+                            inputNameId = "input-username"
+                        />
+                        
+                    </Col>
+
+                    <Col>
+                        
+                    </Col>
+                </Row>
+            </Container>
+
+            
         </div>
     )
 }
