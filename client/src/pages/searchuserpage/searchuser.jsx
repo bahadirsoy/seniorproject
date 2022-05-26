4058305f6ba0e06accb8fb7a595ff39efea19f2f
Bahadir Ustabasi <oxikairos@hotmail.com>
2022-05-26 17:50:34 +0300

    users can be filtered in searchuser page

--- client/src/pages/searchuserpage/searchuser.jsx
+++ client/src/pages/searchuserpage/searchuser.jsx
@@ -2,7 +2,7 @@
 import './searchuser.styles.css'
 
 //import react bootstrap components
-import { Container, Row, Col, InputGroup, FormControl, Table } from 'react-bootstrap';
+import { Container, Row, Col, InputGroup, FormControl, Table, Button } from 'react-bootstrap';
 
 //import fontawesome icons
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
@@ -11,20 +11,33 @@ import { faSearch } from '@fortawesome/free-solid-svg-icons'
 //import react components
 import { useEffect, useState } from 'react';
 
+//import react router components
+import { Link } from 'react-router-dom';
+
 //import Axios
 import Axios from 'axios'
 
 function SearchUser(){
-    //all users
+    //all users and filtered users
     const [users, setUsers] = useState('')
+    const [filteredUsers, setFilteredUsers] = useState('')
 
     useEffect(() => {
         Axios.get('http://localhost:3001/api/getAllUsers', {
             
         }).then((response) => { //feedback from api
             setUsers(response.data)
+            setFilteredUsers(response.data)
         })
     }, [])
+
+    const filterUsers = (e) => {
+        setFilteredUsers(users)
+        const filter = users.filter(user => {
+            return user.username.includes(e.target.value)
+        })
+        setFilteredUsers(filter)
+    }
 
     return(
         <Container className='mt-5'>
@@ -36,9 +49,10 @@ function SearchUser(){
                         </InputGroup.Text>
                         
                         <FormControl
-                        placeholder="Type username to search user"
-                        aria-label="Username"
-                        aria-describedby="basic-addon1"
+                            placeholder="Type username to search user"
+                            aria-label="Username"
+                            aria-describedby="basic-addon1"
+                            onChange={filterUsers}
                         />
                     </InputGroup>
                 </Col>
@@ -49,19 +63,24 @@ function SearchUser(){
                             <tr>
                                 <th>Username</th>
                                 <th>Fullname</th>
-                                <th></th>
+                                <th className='width-20'></th>
                             </tr>
                         </thead>
                         <tbody>
                             {
-                                users ?
-                                users.map(user => {
-                                    console.log(user)
+                                filteredUsers ?
+                                filteredUsers.map(user => {
                                     return(
                                         <tr key={user.userId}>
                                             <td>{user.username}</td>
                                             <td>{user.name + " " + user.surname}</td>
-                                            <td><button></button></td>
+                                            <td className='text-center'>
+                                                <Link to={`/userInformations/${user.username}`}>
+                                                    <Button>
+                                                        Profile
+                                                    </Button>
+                                                </Link>
+                                            </td>
                                         </tr>
                                     )
                                 }) :
